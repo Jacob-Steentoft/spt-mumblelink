5b39bd89ff6ee52fed9a2d657118f76b0e419b91
Jacob Steentoft <jacob@steentoft.net>
2024-08-11 11:21:48 +0200

    feat: added benchmarks

---
+++ ControlFreak.MumbleLink.Benchmarks/Benchmarks/DataWrite.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using SPT.MumbleLink.Services;
+using UnityEngine;
+
+namespace ControlFreak.MumbleLink.Benchmarks.Benchmarks;
+
+[MemoryDiagnoser]
+public class DataWrite
+{
+	private MumbleLinkConnection _connection;
+	private const string Identity = "12345678";
+	private const string Context = "SPT.FIKA.Benchmark";
+	private static readonly Vector3 Up = Vector3.up;
+	private static readonly Vector3 Forward = Vector3.forward;
+	private static readonly Vector3 Position = new(100, 100, 100);
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		_connection = new MumbleLinkConnection(Identity, Context);
+	}
+
+	[Benchmark]
+	public void Update() => _connection.Update(Up, Forward, Position);
+	
+
+	[GlobalCleanup]
+	public void Cleanup()
+	{
+		_connection.Dispose();
+	}
+}
---
+++ ControlFreak.MumbleLink.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using ControlFreak.MumbleLink.Benchmarks.Benchmarks;
+
+namespace ControlFreak.MumbleLink.Benchmarks;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		BenchmarkRunner.Run<DataWrite>(DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator));
+	}
+}
